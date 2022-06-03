2531bf67c01b826481c5c819dfe2259e87d07b1c
Brandon Zubia <brandonz@fb.com>
2022-06-02 22:07:13 -0700

    Created an instance of Async Http Client

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=c60762c624beca97b93230c623d9b12e";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
     }
 }
